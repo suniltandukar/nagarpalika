a2e1e29832b524b6d0ee0a6b83a7db2cef13ad97
shishir <shishir4913120@gmail.com>
2018-07-04 11:43:35 +0545

    commit

--- nagarpalika/src/main/java/com/nagarpalika/dao/HouseLandDao.java
+++
@@ -1,9 +0,0 @@
-package com.nagarpalika.dao;
-
-import com.nagarpalika.model.HouseLandDetailModel;
-
-public interface HouseLandDao {
-
-	public boolean insertHouseLand(HouseLandDetailModel hld);
-
-}
