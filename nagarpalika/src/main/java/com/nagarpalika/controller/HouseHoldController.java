31e4657d117961945d19c911bf0d780fc3d1ae41
Sunil <Sunil@DESKTOP-K3SG9MG>
2018-07-08 10:47:24 +0545

    sunil

---
+++ nagarpalika/src/main/java/com/nagarpalika/controller/HouseHoldController.java
@@ -0,0 +1,26 @@
+package com.nagarpalika.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nagarpalika.model.HouseHoldDetailModel;
+import com.nagarpalika.service.HouseHoldService;
+
+@Controller
+@RequestMapping("/houseHold")
+public class HouseHoldController {
+	
+	@Autowired
+	HouseHoldService hhs;
+	
+	@RequestMapping(value="/insert")
+	public String insertHouseHold(Model model, @ModelAttribute HouseHoldDetailModel hm)
+	{
+		hhs.insertHouseHold(hm);
+		return "";
+	}
+
+}
---
+++ nagarpalika/src/main/java/com/nagarpalika/dao/HouseHoldDao.java
@@ -0,0 +1,8 @@
+package com.nagarpalika.dao;
+
+import com.nagarpalika.model.HouseHoldDetailModel;
+
+public interface HouseHoldDao {
+
+	public boolean insertHouseHold(HouseHoldDetailModel hm);
+}
